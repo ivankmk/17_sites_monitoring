e593aa4aa168a8e3592b2d7aca07726d400c1660
ivankmk <kumeyko@hotmail.com>
2018-04-07 18:58:36 +0200

    script created

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Script will check web sites from your txt file and revert two results: Is the server reply status 200 and is the domain prepaid for at least one month from the current date.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+
+
+# How to launch
+
+```bash
+
+$ python check_sites_health.py list.txt # possibly requires call of python3 executive instead of just python
+----------------------------------------
+Web site: http://devman.org
+Is server return status 200 ?: Yes
+If domain prepaid for the next month?: Yes
+----------------------------------------
+Web site: http://lenta.ru
+Is server return status 200 ?: Yes
+If domain prepaid for the next month?: Yes
+----------------------------------------
+Web site: http://le23524nta.ru
+Is server return status 200 ?: Site not exist
+If domain prepaid for the next month?: Site not exist
+```
+
+The same with Windows environment;
+
+
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,52 @@
+import whois
+import requests
+from datetime import datetime, timedelta
+import sys
+
+
 def load_urls4check(path):
-    pass
+    with open(path, mode='r') as file:
+        return [site.strip() for site in file]
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        if requests.request('GET', url).status_code == 200:
+            return 'Yes'
+        else:
+            return 'No'
+    except requests.exceptions.ConnectionError:
+        return 'Site not exist'
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    try:
+        date = whois.whois(domain_name).expiration_date
+        if type(date) == list:
+            return date[0]
+        else:
+            return date
+    except whois.socket.gaierror:
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_with_sites = sys.argv[1]
+        month_ahead = datetime.now() + timedelta(weeks=4)
+        list_with_sites = load_urls4check(file_with_sites)
+        for site in list_with_sites:
+            expiration_date = get_domain_expiration_date(site)
+            print('-'*40)
+            print('Web site: {}'.format(site))
+            print('Is server return status 200 ?: {}'
+                  .format(is_server_respond_with_200(site)))
+            if expiration_date is None:
+                print('If domain prepaid for the next month?: Site not exist')
+            elif expiration_date > month_ahead:
+                print('If domain prepaid for the next month?: Yes')
+            elif expiration_date <= month_ahead:
+                print('If domain prepaid for the next month?: No')
+        print('\n')
+    except (FileNotFoundError, IndexError):
+        print('Please specify or check your file with sites')
